07b829843a256bbedd63fe78b546e4583e481ddd
neegar-09 <nigarabdullayeva0906@gmail.com>
2024-02-19 15:09:39 +0400

    sbxs

---
+++ src/Pages/SignIn.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+
+const SignIn = () => {
+  return (
+    <>
+    <div>SignIn</div>
+    <Link to="/signup" >SignUp</Link>
+    </>
+  )
+}
+
+export default SignIn
